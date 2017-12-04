a3a3a4bfd9357e03ad931759fff2b5a8162a5455
volkovandr <andrey.v.volkov@gmail.com>
2017-12-04 22:12:03 +0100

    Made nicer string representation with line numbers

--- src/main/py/eel_parser.py
+++ src/main/py/eel_parser.py
@@ -4,15 +4,25 @@ Parser of eel code
 '''
 
 
+import math
+
+
 class token:
 
-    def __init__(self, filename, lines):
+    def __init__(self, filename, lines, start_from):
         self.filename = filename
         self.lines = lines
+        self.start_from = start_from
 
     def __repr__(self):
+        line_numbers = list(
+            range(self.start_from, self.start_from + len(self.lines)))
+        pad = int(math.log10(line_numbers[-1])) + 1
+        pattern = "{:>" + str(pad) + "}: {}"
         return "Code from {}:\n{}\n".format(
-            self.filename, "\n".join(self.lines))
+            self.filename, "\n".join(
+                [pattern.format(a[0], a[1])
+                 for a in zip(line_numbers, self.lines)]))
 
 
 def parse_eel_code(lines, filename):
--- src/test/py/test_eel_parser.py
+++ src/test/py/test_eel_parser.py
@@ -8,11 +8,11 @@ import textwrap
 class TestEelParser(TestCase):
 
     def test_token_can_represent_itself(self):
-        toc = eel_parser.token("some_file.eel", ["line one", "line two"])
+        toc = eel_parser.token("some_file.eel", ["line one", "line two"], 9)
         str_repr = str(toc)
         expected = textwrap.dedent('''\
             Code from some_file.eel:
-            line one
-            line two
+             9: line one
+            10: line two
             ''')
         self.assertEqual(str_repr, expected)
